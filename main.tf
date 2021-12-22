39fb6142409cefc89daaa1896884b45958aedeba
abhitahaa <96439641+abhitahaa@users.noreply.github.com>
2021-12-21 22:53:43 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,19 +2,28 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "vpc_cidr_block" {}
+variable "subnet_cidr_block" {}
+variable "env_prefix" {}
+variable "my_ip" {}
+variable "instance_type" {}
+variable "public_key_location" {}
+
+
+
 #VPC creation
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr_block
 
   tags = {
-    Name = "main"
+    Name : "${var.env_prefix}-mainvpc"
   }
 }
 
 #Create Subnet
 
 resource "aws_subnet" "public_subnet" {
-  cidr_block        = "10.0.10.0/24"
+  cidr_block        = var.subnet_cidr_block
   vpc_id            = aws_vpc.main.id
   availability_zone = "us-east-1a"
 }
@@ -44,6 +53,15 @@ resource "aws_route_table" "example" {
     Name = "example"
   }
 }
+
+
+resource "aws_route_table_association" "a-rtb-subnet" {
+
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.example.id
+
+}
+
 
 # Create Security Group
 resource "aws_security_group" "jenkins_test_sg" {
@@ -155,7 +173,7 @@ resource "aws_security_group" "sonar_test_sg" {
   }
 }
 
-
+#allowing jenkins secuity group in nexus security group. here source is jenkins and destination is nexus security group. 
 resource "aws_security_group_rule" "allow_jenkins_test_sg" {
 
   type                     = "ingress"
@@ -163,8 +181,8 @@ resource "aws_security_group_rule" "allow_jenkins_test_sg" {
   from_port                = 8081
   to_port                  = 8081
   protocol                 = "tcp"
-  security_group_id        = aws_security_group.nexusrepo_test_sg.id
-  source_security_group_id = aws_security_group.jenkins_test_sg.id
+  security_group_id        = aws_security_group.nexusrepo_test_sg.id #to
+  source_security_group_id = aws_security_group.jenkins_test_sg.id   #from
 
 
 }
@@ -207,21 +225,94 @@ resource "aws_security_group_rule" "allow_jenkins_from_sonar_sg" {
 
 }
 
+resource "aws_key_pair" "ssh-key" {
+
+  key_name   = "server-key"
+  public_key = file(var.public_key_location) #file location for your id_rsa.pub file
+}
+
+
+
 resource "aws_instance" "jenkins_server" {
 
-    ami = 
-    instance_type = t2.micro
-    subnet_id = 
-    vpc_id = 
-    
+  ami           = "ami-04505e74c0741db8d"
+  instance_type = var.instance_type[0]
+  #vpc_id                      = aws_vpc.main.id
+  subnet_id                   = aws_subnet.public_subnet.id
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.jenkins_test_sg.id]
+  key_name                    = aws_key_pair.ssh-key.key_name
 
-    tags = {
-        Name = "jenkins server"
-    }
+  user_data = file("jenkins-setup.sh")
+
+
+  tags = {
+    Name = "${var.env_prefix}-jenkins_server"
+  }
 
 
 }
-*/
+
+output "ec2_public_ip" {
+  value = aws_instance.jenkins_server.public_ip
+}
+
+resource "aws_instance" "sonar_server" {
+
+  ami           = "ami-04505e74c0741db8d"
+  instance_type = var.instance_type[1]
+  #vpc_id                      = aws_vpc.main.id
+  subnet_id                   = aws_subnet.public_subnet.id
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.sonar_test_sg.id]
+  key_name                    = aws_key_pair.ssh-key.key_name
+
+  user_data = file("sonar-setup.sh")
+
+
+  tags = {
+    Name = "${var.env_prefix}-sonar_server"
+  }
+
+
+}
+
+resource "aws_instance" "nexus_server" {
+
+  ami           = "ami-0ed9277fb7eb570c9"
+  instance_type = var.instance_type[1]
+  #vpc_id                      = aws_vpc.main.id
+  subnet_id                   = aws_subnet.public_subnet.id
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.nexusrepo_test_sg.id]
+  key_name                    = aws_key_pair.ssh-key.key_name
+
+  user_data = file("nexus-setup.sh")
+
+
+  tags = {
+    Name = "${var.env_prefix}-nexus_server"
+  }
+
+
+}
+
+
+
+
+
+output "ec2_jenkinspublic_ip" {
+  value = aws_instance.jenkins_server.public_ip
+}
+
+output "ec2_sonarpublic_ip" {
+  value = aws_instance.sonar_server.public_ip
+}
+
+output "ec2_nexuspublic_ip" {
+  value = aws_instance.nexus_server.public_ip
+}
+
 
 /*
 resource "aws_security_group" "jenkins_sg" {
